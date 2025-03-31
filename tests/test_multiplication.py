1ad1647e72a710f86c8466ccef7956c180218780
Brian Martinez <100006298+bamart-dev@users.noreply.github.com>
2025-03-31 14:11:01 -0700

    initial commit

--- tests/test_multiplication.py
+++ tests/test_multiplication.py
@@ -1,8 +1,7 @@
-import multiplication
+import calculator.multiplication as multiplication
 
 def test_multiplication():
     # Assert
     assert multiplication.perform_operation(1, 1) == 1
     assert multiplication.perform_operation(10, 20) == 200
     assert multiplication.perform_operation(55, 11) == 605
-    
